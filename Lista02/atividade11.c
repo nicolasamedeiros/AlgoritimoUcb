c37828d92eefafcf7102ad9243bcf2c40dba9889
nicolasamedeiros <61924838+nicolasamedeiros@users.noreply.github.com>
2022-09-24 19:00:01 -0300

    atividade11

--- Lista02/atividade11.c
+++ Lista02/atividade11.c
@@ -4,14 +4,15 @@
 #define PI 3.14159;
 
 int main () {
-    float raio, area, volume;
+    int raio;
+    float volume, area;
 
-    printf("Digite o valor do raio: ");
-    scanf("%f", &raio);
+    printf("Digite o raio: ");
+    scanf("%d", &raio);
 
     volume = (4/3.0) * PI * pow(raio, 3);
     area = 4 * PI * pow(raio, 2);
 
-    printf("O valor da area e: %f", area);
-    printf("O valor do volume da area e %f", volume);
+    printf("O volume da esfera e %f", volume);
+    printf("A area da esfera e %f", area);
 }
