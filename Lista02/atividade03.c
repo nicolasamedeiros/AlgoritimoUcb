83435bc109e8ebcb1409edb0c7c32d30befae856
Nicolas Almeida Medeiros <61924838+nicolasamedeiros@users.noreply.github.com>
2022-10-09 21:58:07 -0300

    Atualizei a atividade03 lista02

--- Lista02/atividade03.c
+++ Lista02/atividade03.c
@@ -4,25 +4,30 @@
 int main (){
     int valor, bolean;
     float num1, num2;
-    char letra[1], str[20];
+    char letra, str[20]; 
 
     printf("Digite um numero inteiro: ");
     scanf("%d", &valor);
     
     printf("Digite dois numeros reais: ");
     scanf("%f%f", &num1, &num2);
+    fflush(stdin); 
     
     printf("Digite uma letra: ");
     scanf("%c", &letra);
+    fflush(stdin);
 
     printf("Digite uma string: ");
     scanf("%s", str);
+    fflush(stdin);
       
     printf("Digite 1 para verdadeiro e 0 para falso: ");
     scanf("%d", &bolean);
 
+    printf("--------------------------------------------\n");
+
     printf("Numero Inteiro: %d\n", valor);
-    printf("Numeros Reais: \n %f \n %f\n", num1, num2);
+    printf("Numeros Reais: \n %.2f \n %.2f\n", num1, num2);
     printf("Letra: %c\n", letra);
     printf("String: %s\n", str);
     if (bolean == 1) {
