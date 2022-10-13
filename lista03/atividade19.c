1a0aa713a24db7bb75101b7c14ce782a58069a77
Nicolas Almeida Medeiros <61924838+nicolasamedeiros@users.noreply.github.com>
2022-10-13 17:14:48 -0300

    atividade10 lista 02

---
+++ lista03/atividade19.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main () {
+    float n1, n2 , n3, n4, media;
+
+    printf("Digite a primeira nota do aluno: ");
+    scanf("%.1f", &n1);
+    fflush(stdin);
+    printf("Digite a segunda nota do aluno: ");
+    scanf("%.1f", &n2);
+    fflush(stdin);
+    printf("Digite a terceira nota do aluno: ");
+    scanf("%.1f", &n3);
+    fflush(stdin);
+    printf("Digite a quarta nota do aluno: ");
+    scanf("%.1f", &n4);
+    fflush(stdin);
+
+    media = (n1*2) + (n2*3) + (n3*4) + (n4*1) / (2 + 3 + 4 + 1);
+
+    printf("Media: %.2f", media);
+}
