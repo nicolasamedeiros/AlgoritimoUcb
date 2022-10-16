0abb055c9cb2742a1e34233e901d0aefa87eb072
nicolasamedeiros <61924838+nicolasamedeiros@users.noreply.github.com>
2022-10-16 11:41:34 -0300

    Atividade_19 lista_03

--- lista03/atividade19.c
+++ lista03/atividade19.c
@@ -1,23 +1,36 @@
 #include<stdio.h>
 
 int main () {
-    float n1, n2 , n3, n4, media;
+    float n1, n2 , n3, n4, media, exame, n_final;
 
-    printf("Digite a primeira nota do aluno: ");
-    scanf("%.1f", &n1);
-    fflush(stdin);
-    printf("Digite a segunda nota do aluno: ");
-    scanf("%.1f", &n2);
-    fflush(stdin);
-    printf("Digite a terceira nota do aluno: ");
-    scanf("%.1f", &n3);
-    fflush(stdin);
-    printf("Digite a quarta nota do aluno: ");
-    scanf("%.1f", &n4);
-    fflush(stdin);
+    printf("Digite as quatro notas do aluno: ");
+    scanf("%f%f%f%f", &n1, &n2, &n3, &n4);
 
-    media = (n1*2) + (n2*3) + (n3*4) + (n4*1) / (2 + 3 + 4 + 1);
+    media = (n1 * 2 + n2 * 3 + n3 * 4 + n4 * 1) / 10;
 
-    printf("Media: %.2f", media);
-    printf("Salve");
+    printf("Media: %.1f\n", media);
+
+    if (media >= 7.0) {
+        printf("Aluno Aprovado.");
+    }
+        else if(media >= 5.0 && media <= 6.9) {
+            printf("Aluno em exame\n");
+            
+            printf("Nota de exame: ");
+            scanf("%.1f", &exame);
+
+            n_final = media + exame / 2;
+
+            if (n_final >= 5.0) {
+                printf("Aluno Aprovado\n");
+                printf("Media: %.1f", n_final);
+            } 
+            else{
+                printf("Aluno Reprovado\n");
+                printf("Media: %.1f", n_final);
+            }
+        } 
+            else {
+                printf("Aluno Reprovado");
+            }
 }
